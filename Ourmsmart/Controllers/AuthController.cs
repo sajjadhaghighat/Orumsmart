7a5dc289852d35fe9186c69ea3c28c9b1ed4a769
sajjad <hagigat.sajjad.3@gmail.com>
2020-11-29 18:49:24 +0330

    panel profile

---
+++ Ourmsmart/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Ourmsmart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ourmsmart.Controllers
+{
+    public class AuthController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Auth/login")]
+        public IHttpActionResult login(Users user)
+        {
+            if (user.Username == "sajjad" && user.Password == "137596")
+            {
+                return Json(new { success = true, message = "You Login Successfully." });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Your Information is Incorrect." });
+            }
+        }
+    }
+}
---
+++ Ourmsmart/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Ourmsmart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ourmsmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
