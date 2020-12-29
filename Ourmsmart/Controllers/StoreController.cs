3662e7b70b0c1cb2fd1b7cee20fe69224d479e07
sajjad <hagigat.sajjad.3@gmail.com>
2020-12-29 16:23:34 +0330

    work on store page

--- Ourmsmart/Controllers/StoreController.cs
+++ Ourmsmart/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ourmsmart.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,46 @@ using System.Web.Mvc;
 
 namespace Ourmsmart.Controllers
 {
+    
     public class StoreController : Controller
     {
+        private VIRADB db = new VIRADB();
         // GET: Store
-        public ActionResult Index()
+        public ActionResult Index(string searchBy,string search,int? page)
         {
+            var q = from a in db.FAProducts select a;
+            return View(q);
+        }
+
+        public ActionResult Products()
+        {
+            var q = from a in db.FAProducts select a;
+            return View(q);
+        }
+
+        public ActionResult CreateProduct()
+        {
+            
             return View();
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult CreateProduct(FAProduct product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new { message = "" , title = ""});
+            }
+            try
+            {
+                db.FAProducts.Add(product);
+                db.SaveChanges();
+                return Json(new { message = "", title = "" });
+            }
+            catch (Exception)
+            {
+                return Json(new { message = "", title = "" });
+            }
         }
     }
 }
